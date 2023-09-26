e0cda17893e9b19a2e556048afa9500278a466af
Saad Beidouri <saad.beidouri@etu.unice.fr>
2023-09-26 15:49:06 +0200

    BrowseTest Class

---
+++ src/test/java/fr/unice/polytech/biblio/BrowseTest.java
@@ -0,0 +1,25 @@
+package fr.unice.polytech.biblio;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BrowseTest {
+
+    @Given("{string} is on the menu page")
+    public void is_on_the_menu_page(String string) {
+        //TODO
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("{string} requests the list of menus")
+    public void requests_the_list_of_menus(String string) {
+        //TODO
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should display a list of menus for {string}")
+    public void the_system_should_display_a_list_of_menus_for(String string) {
+        //TODO
+        throw new io.cucumber.java.PendingException();
+    }
+}
