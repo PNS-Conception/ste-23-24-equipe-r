1999ebada6d2568dafed1af40fa0cb0eca17eab2
Saad Beidouri <saad.beidouri@etu.unice.fr>
2023-10-11 19:45:38 +0200

    #5 setup US

---
+++ src/test/java/fr/unice/polytech/steats/cucumber/CheckFoodDeliveryStatusStep.java
@@ -0,0 +1,22 @@
+package fr.unice.polytech.steats.cucumber;
+
+import io.cucumber.java.en.*;
+
+public class CheckFoodDeliveryStatusStep {
+
+    @Given("an existing food {string}")
+    public void an_existing_food(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the {string} selects the {string} and requests the delivery {string}")
+    public void the_selects_the_and_requests_the_delivery(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the application displays {string} of the order")
+    public void the_application_displays_of_the_order(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
