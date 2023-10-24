1713c366d6bb3dee752852a1240afbf9af013f9f
bilal-mesaouri <bilmsr.bm@gmail.com>
2023-10-24 11:12:10 +0200

    #8 adding new restaurant

---
+++ src/main/java/fr/unice/polytech/steats/Exceptions/RestaurantAdditionException.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.steats.Exceptions;
+
+public class RestaurantAdditionException extends Exception {
+    public RestaurantAdditionException(String message) {
+        super(message);
+    }
+}
