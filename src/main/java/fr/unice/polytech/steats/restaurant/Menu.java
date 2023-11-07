01e3f807c9f1a5b1be2a7edfd964cbe38a66591f
bilal-mesaouri <bilmsr.bm@gmail.com>
2023-11-07 12:50:08 +0100

    #22 Customizing menu

--- src/main/java/fr/unice/polytech/steats/restaurant/Menu.java
+++ src/main/java/fr/unice/polytech/steats/restaurant/Menu.java
@@ -1,11 +1,17 @@
 package fr.unice.polytech.steats.restaurant;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class Menu {
     private UUID id;
     private String menuName;
     private double price;
+
+    private Map<String, List<String>> options = new HashMap<>();
+    private Map<String, String> selectedOptions = new HashMap<>();
 
     public Menu(String menuName, double price) {
         this.menuName = menuName;
@@ -23,5 +29,21 @@ public class Menu {
 
     public double getPrice() {
         return price;
+    }
+
+    public Map<String, List<String>> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Map<String, List<String>> options) {
+        this.options = options;
+    }
+
+    public Map<String, String> getSelectedOptions() {
+        return selectedOptions;
+    }
+
+    public void setSelectedOptions(Map<String, String> selectedOptions) {
+        this.selectedOptions = selectedOptions;
     }
 }
