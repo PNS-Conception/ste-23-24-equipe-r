5b45af4daafe6a06ab93eea0885bde97e38ea269
bilal-mesaouri <bilmsr.bm@gmail.com>
2023-11-07 13:07:32 +0100

    Revert "Merge remote-tracking branch 'origin/dev' into US22"
    This reverts commit 43ba786bd00d0a2dc70db12efa169af71c2b51ae, reversing
    changes made to 2a1223001ebeb9a70746d9aa6031d14f793719f9.

--- src/main/java/fr/unice/polytech/steats/restaurant/Menu.java
+++ src/main/java/fr/unice/polytech/steats/restaurant/Menu.java
@@ -1,8 +1,6 @@
 package fr.unice.polytech.steats.restaurant;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Menu {
@@ -10,12 +8,15 @@ public class Menu {
     private String menuName;
     private double price;
 
-    private Map<String, List<String>> options = new HashMap<>();
-    private Map<String, String> selectedOptions = new HashMap<>();
-
     public Menu(String menuName, double price) {
+        this.id = UUID.randomUUID();
         this.menuName = menuName;
         this.price = price;
+    }
+    public Menu(Menu menu){
+        this.id = UUID.randomUUID();
+        this.menuName = menu.getMenuName();
+        this.price = menu.getPrice();
     }
 
     // Getters and Setters
@@ -31,19 +32,15 @@ public class Menu {
         return price;
     }
 
-    public Map<String, List<String>> getOptions() {
-        return options;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Double.compare(menu.price, price) == 0 && menuName.equals(menu.menuName);
     }
-
-    public void setOptions(Map<String, List<String>> options) {
-        this.options = options;
-    }
-
-    public Map<String, String> getSelectedOptions() {
-        return selectedOptions;
-    }
-
-    public void setSelectedOptions(Map<String, String> selectedOptions) {
-        this.selectedOptions = selectedOptions;
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuName, price);
     }
 }
